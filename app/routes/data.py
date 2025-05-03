cb4c965c1c8b552d662e38dd338246c1a6ebcf55
edenCharis <edenngouanda@gmail.com>
2025-05-03 21:36:37 +0100

    shaping the datascientist role

--- app/routes/data.py
+++ app/routes/data.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from ..models import Dataset
 from ..utils import role_required
@@ -22,5 +22,30 @@ def dashboard():
 @role_required('data-scientist')
 def upload():
 
-    datasets = Dataset.query.filter_by(user_id=current_user.id).all()
-    return render_template('data/index.html', datasets=datasets, user=current_user)
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and file.filename.endswith('.csv'):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+
+            # Perform basic EDA
+            df = pd.read_csv(filepath)
+            num_rows, num_cols = df.shape
+
+            # Save to database
+            dataset = Dataset(
+                name=filename,
+                file_path=filepath,
+                num_rows=num_rows,
+                num_cols=num_cols,
+                upload_date=datetime.now()
+            )
+            db.session.add(dataset)
+            db.session.commit()
+
+            flash('Dataset uploaded successfully!', 'success')
+            return redirect(url_for('view_datasets'))
+        else:
+            flash('Invalid file type. Please upload a CSV file.', 'danger')
+    return render_template('upload.html')
